fd10e465411b7c6f9060f55772f0594b45d2ae7e
nh0126 <nh0126@sookmyung.ac.kr>
2023-11-16 16:10:01 +0900

    실습5

--- main.c
+++ main.c
@@ -2,23 +2,20 @@
 #include <stdlib.h>
 
 int main(void) {
-	int i;
-	int grade[5];
-	int average = 0; 
+	char *pc;
+	int *pi;
+	double *pd;
 	
-	for (i=0;i<5;i++)
-	{
-		printf("input value (%i) : ",i);
-		scanf("%d",&grade[i]);
-	}
+	pc = (char *)10000;
+	pi = (int*)10000;
+	pd = (double *)10000;
 	
-	int ave = 0;
-	for (i=0;i<5;i++)
-	{
-		printf("grade[%i] = %i\n", i, *(grade+i)); //grade[i]는 배열의 문법, *(grade+1)는 포인터 문법 
-		average += *(grade+i);
-	}
-	printf("average : %i", average/5);
+	printf("증가 전 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+	
+	pc++;
+	pi++;
+	pd++;
+	printf("증가 후 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
 	
 	system("PAUSE");
 	return 0;
