493eefb0194a7ba1ad00c264eb99e9a732fef5ce
nh0126 <nh0126@sookmyung.ac.kr>
2023-11-16 15:54:31 +0900

    실습4

--- main.c
+++ main.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swap(int *x, int *y){
-	int temp;
-	
-	temp = *x;
-	*x = *y;
-	*y = temp;
-}
 int main(void) {
-	int a = 3;
-	int b = 5;
-	swap(&a, &b);
-	print("a:%i, b:%i\n", a, b);
+	int i;
+	int grade[5];
+	int average = 0; 
+	
+	for (i=0;i<5;i++)
+	{
+		printf("input value (%i) : ",i);
+		scanf("%d",&grade[i]);
+	}
+	
+	int ave = 0;
+	for (i=0;i<5;i++)
+	{
+		printf("grade[%i] = %i\n", i, *(grade+i)); //grade[i]는 배열의 문법, *(grade+1)는 포인터 문법 
+		average += *(grade+i);
+	}
+	printf("average : %i", average/5);
 	
 	system("PAUSE");
 	return 0;
